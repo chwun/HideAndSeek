9f59dd10452f52447a5f7bd388b4f1e97af80117
chwun <ch.wunderlich@yahoo.de>
2021-01-19 18:36:36 +0100

    add game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace HideAndSeek
+{
+	public class GameManager : MonoBehaviour
+	{
+		public GameObject PlayerPrefab;
+		public GameObject SpawnPosition;
+
+		private GameObject player;
+
+		void Start()
+		{
+			SpawnPlayer();
+		}
+
+		private void SpawnPlayer()
+		{
+			player = Instantiate(PlayerPrefab, SpawnPosition.transform.position, Quaternion.identity);
+		}
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,85 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace HideAndSeek
 {
-	private CharacterController characterController;
-
-	private float currentXRotation = 0f;
-	private Vector3 currentVelocity;
-
-	public Camera PlayerCam;
-	public float RotationSpeed = 100f;
-	public float WalkingSpeed = 4f;
-	public float RunningSpeed = 8f;
-	public float JumpHeight = 1f;
-	public float Gravity = -9.81f;
-
-
-	// Start is called before the first frame update
-	void Start()
+	public class PlayerMovement : MonoBehaviour
 	{
-		characterController = GetComponent<CharacterController>();
+		private CharacterController characterController;
 
-		Cursor.lockState = CursorLockMode.Locked;
-	}
+		private float currentXRotation = 0f;
+		private Vector3 currentVelocity;
 
-	// Update is called once per frame
-	void Update()
-	{
-		Rotate();
-		Move();
-	}
+		public Camera PlayerCam;
+		public float RotationSpeed = 100f;
+		public float WalkingSpeed = 4f;
+		public float RunningSpeed = 8f;
+		public float JumpHeight = 1f;
+		public float Gravity = -9.81f;
 
-	private void Move()
-	{
-		bool isGrounded = characterController.isGrounded;
-		if (isGrounded && currentVelocity.y < 0)
+
+		// Start is called before the first frame update
+		void Start()
 		{
-			currentVelocity.y = 0f;
+			characterController = GetComponent<CharacterController>();
+
+			Cursor.lockState = CursorLockMode.Locked;
 		}
 
-		bool isRunning = Input.GetKey(KeyCode.LeftShift);
-
-		float horizontalMove = Input.GetAxis("Horizontal");
-		float verticalMove = Input.GetAxis("Vertical");
-
-		Vector3 moveDirection = (transform.forward * verticalMove + transform.right * horizontalMove) * Time.deltaTime;
-
-		if (isRunning)
+		// Update is called once per frame
+		void Update()
 		{
-			moveDirection *= RunningSpeed;
-		}
-		else{
-			moveDirection *= WalkingSpeed;
+			Rotate();
+			Move();
 		}
 
-		if (!isGrounded)
+		private void Move()
 		{
-			moveDirection *= 0.7f;
+			bool isGrounded = characterController.isGrounded;
+			if (isGrounded && currentVelocity.y < 0)
+			{
+				currentVelocity.y = 0f;
+			}
+
+			bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+			float horizontalMove = Input.GetAxis("Horizontal");
+			float verticalMove = Input.GetAxis("Vertical");
+
+			Vector3 moveDirection = (transform.forward * verticalMove + transform.right * horizontalMove) * Time.deltaTime;
+
+			if (isRunning)
+			{
+				moveDirection *= RunningSpeed;
+			}
+			else
+			{
+				moveDirection *= WalkingSpeed;
+			}
+
+			if (!isGrounded)
+			{
+				moveDirection *= 0.7f;
+			}
+
+			if (isGrounded && Input.GetButton("Jump"))
+			{
+				currentVelocity.y += JumpHeight * -0.8f * Gravity;
+			}
+
+			currentVelocity.y += 2f * Gravity * Time.deltaTime;
+
+			characterController.Move(moveDirection + currentVelocity * Time.deltaTime);
 		}
 
-		if (isGrounded && Input.GetButton("Jump"))
+		private void Rotate()
 		{
-			currentVelocity.y += JumpHeight * -0.8f * Gravity;
+			float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+			float mouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+
+			currentXRotation -= mouseY;
+			currentXRotation = Mathf.Clamp(currentXRotation, -90f, 90f);
+
+			PlayerCam.transform.localRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
+
+			transform.Rotate(Vector3.up * mouseX);
 		}
-
-		currentVelocity.y += 2f * Gravity * Time.deltaTime;
-
-		characterController.Move(moveDirection + currentVelocity * Time.deltaTime);
-	}
-
-	private void Rotate()
-	{
-		float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
-		float mouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
-
-		currentXRotation -= mouseY;
-		currentXRotation = Mathf.Clamp(currentXRotation, -90f, 90f);
-
-		PlayerCam.transform.localRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
-
-		transform.Rotate(Vector3.up * mouseX);
 	}
 }
