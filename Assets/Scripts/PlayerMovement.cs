7547c613aa89b9d20afc03c49b747f9a5fb55b00
Jannis Wagner <jannis.wagner.de@gmail.com>
2021-01-29 18:55:38 +0100

    updated PlayerMovement: crouching is possible on C. Sneaking on Ctrl. Running on Shift. Fixed levetating on ceiling by checking hits between player head an collider with raycasting.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,15 @@ namespace HideAndSeek
 
         private float currentXRotation = 0f;
         private Vector3 currentVelocity;
+        private Vector3[] scanDirections;
+		private float normalHeight;
+		private float croucheHeight;
 
         public Camera PlayerCam;
         public float RotationSpeed = 100f;
-        public float PlayerAcceleration = 10f;
-        public float WalkingSpeed = 4f;
-        public float RunningSpeed = 8f;
+		public float WalkingSpeed = 5f;
+		public float RunningSpeed = 10f;
+		public float SneakingSpeed = 2f;
         public float JumpHeight = 1.1f;
         public float Gravity = -9.81f;
 
@@ -22,11 +25,12 @@ namespace HideAndSeek
         // Start is called before the first frame update
         void Start()
         {
-            characterController = GetComponent<CharacterController>();
+			characterController = GetComponent<CharacterController>();
+			normalHeight = transform.localScale.y;
+			croucheHeight = normalHeight * 3f/4f;
+			InitUpwardsCollisionSystem();
 
-            Cursor.lockState = CursorLockMode.Locked;
-
-            characterController.detectCollisions = true;
+			Cursor.lockState = CursorLockMode.Locked;
         }
 
         // Update is called once per frame
@@ -39,56 +43,128 @@ namespace HideAndSeek
         
 
         private void Move()
-        {
-            bool isGrounded = characterController.isGrounded;
-            if (isGrounded && currentVelocity.y < 0)
-            {
-                currentVelocity.y = 0f;
-            }
+		{
+			bool isGrounded = characterController.isGrounded;
+			
+			// Player Inputs
+			bool isRunning = Input.GetKey(KeyCode.LeftShift);
+			bool isSneaking = Input.GetKey(KeyCode.LeftControl);
+			bool isCrouching = Input.GetKey(KeyCode.C);
 
-            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+			float horizontalMove = Input.GetAxis("Horizontal");
+			float verticalMove = Input.GetAxis("Vertical");
 
-            float horizontalMove = Input.GetAxis("Horizontal");
-            float verticalMove = Input.GetAxis("Vertical");
 
-            Vector3 moveDirection = (transform.forward * verticalMove + transform.right * horizontalMove) * Time.deltaTime;
+			if (isGrounded && currentVelocity.y < 0f)
+			{
+				currentVelocity.y = 0f;
+			}
 
-            if (isRunning)
-            {
-                moveDirection *= RunningSpeed;
-            }
-            else
-            {
-                moveDirection *= WalkingSpeed;
-            }
+			// Handeling player speed and direction
+			Vector3 moveDirection = (transform.forward * verticalMove + transform.right * horizontalMove) * Time.deltaTime;
+			
+			if (isRunning && !isCrouching)
+			{
+				moveDirection *= RunningSpeed;
+			}
+			else if(isSneaking)
+			{
+				moveDirection *= SneakingSpeed;
+			}
+			else
+			{
+				moveDirection *= WalkingSpeed;
+			}
 
-            if (!isGrounded)
-            {
-                moveDirection *= 0.7f;
-            }
+			if (!isGrounded)
+			{
+				moveDirection *= 0.7f;
+			}
 
-            if (isGrounded && Input.GetButton("Jump"))
-            {
-                currentVelocity.y += Mathf.Sqrt(-1f * Gravity * JumpHeight);
-            }
 
-            currentVelocity.y += Gravity * Time.deltaTime;
+			// Changing Player Height while Crouching
+			if (isCrouching && transform.localScale.y != croucheHeight)
+			{
+				transform.localScale = new Vector3(transform.localScale.x, croucheHeight, transform.localScale.z);
+				
+				characterController.enabled = false;
+				int dir = isGrounded ? -1 : 1;
+				transform.position += Vector3.up * normalHeight/4f * dir;
+				characterController.enabled = true;
+			}
+			if (!isCrouching && transform.localScale.y != normalHeight && !checkUpwardsCollisions())
+			{
+				characterController.enabled = false;
+				int dir = isGrounded ? 1 : -1;
+				transform.position += Vector3.up * normalHeight/4f * dir;
+				characterController.enabled = true;
 
-            characterController.Move(moveDirection + currentVelocity * Time.deltaTime);
+				transform.localScale = new Vector3(transform.localScale.x, normalHeight, transform.localScale.z);
+			}
+			
+			// Check if crouching through height not through button, to change speed
+			if (transform.localScale.y == croucheHeight)
+			{
+				moveDirection *= 0.5f;
+			}
 
-        }
+			// Handeling jumps, gravity and head collisions
+			if (isGrounded && Input.GetButton("Jump")) // TODO: Check for Slope Angle to avoid jumping further on a slope. Consider sliding down to steep slopes. [Check hit.normal on func OnControllerColliderHit]
+			{
+				currentVelocity.y -= JumpHeight/1.55f * Gravity;
+			}
 
-        private void Rotate()
-        {
-            float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+			currentVelocity.y += 2f * Gravity * Time.deltaTime;
 
-            currentXRotation -= mouseY;
-            currentXRotation = Mathf.Clamp(currentXRotation, -90f, 90f);
+			if (checkUpwardsCollisions() && currentVelocity.y > 0f)
+			{
+				currentVelocity.y = 0f;
+			}
 
-            PlayerCam.transform.localRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
+			// Move the player
+			characterController.Move(moveDirection + currentVelocity * Time.deltaTime);
+		}
 
-            transform.Rotate(Vector3.up * mouseX);
-        }
-    }
+		private void Rotate()
+		{
+			float mouseX = Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime;
+			float mouseY = Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime;
+
+			currentXRotation -= mouseY;
+			currentXRotation = Mathf.Clamp(currentXRotation, -90f, 90f);
+
+			PlayerCam.transform.localRotation = Quaternion.Euler(currentXRotation, 0f, 0f);
+
+			transform.Rotate(Vector3.up * mouseX);
+		}
+
+		private bool checkUpwardsCollisions()
+		{
+			RaycastHit hit;
+			Vector3 headPoint = transform.position + Vector3.up * (characterController.height/2 - characterController.radius);
+			foreach(Vector3 dir in scanDirections)
+			{
+				if (Physics.Raycast(headPoint, dir, out hit, 2*JumpHeight))
+				{
+					// Debug.DrawRay(headPoint, hit.point - headPoint, Color.red);
+					if (hit.distance <=  characterController.radius + 0.1f)
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		private void InitUpwardsCollisionSystem()
+		{
+			scanDirections = new Vector3[5] {
+				Vector3.up,
+				new Vector3(0.5f, 1f, 0f),
+				new Vector3(-0.5f, 1f, 0f),
+				new Vector3(0f, 1f, 0.5f),
+				new Vector3(0f, 1f, -0.5f)
+				};
+		}
+	}
 }
