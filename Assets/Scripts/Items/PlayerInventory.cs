e3804abe126f87b8992f68f468e16e08550568ba
chwun <ch.wunderlich@yahoo.de>
2021-01-30 14:56:13 +0100

    inventory: select slots, add item

--- Assets/Scripts/Items/PlayerInventory.cs
+++ Assets/Scripts/Items/PlayerInventory.cs
@@ -2,19 +2,60 @@ namespace HideAndSeek.Items
 {
 	public class PlayerInventory
 	{
-		public Item Item1 { get; private set; }
+		private const int InventorySize = 3;
 
-		public Item Item2 { get; private set; }
+		public Item[] Items { get; private set; }
 
-		public Item Item3 { get; private set; }
+		public int SelectedItemSlot { get; private set; }
 
-		public PlayerInventory(){
-
+		public PlayerInventory()
+		{
+			Items = new Item[InventorySize];
+			SelectedItemSlot = 0;
 		}
 
 		public void AddItem(Item item)
 		{
+			int nextFreeItemSlot = GetNextFreeItemSlot();
 
+			if (nextFreeItemSlot > -1)
+			{
+				// if free slot is found: add item to this slot
+				Items[nextFreeItemSlot] = item;
+			}
+			else
+			{
+				// if no free slot: replace currently select item slot
+				Items[SelectedItemSlot] = item;
+			}
+		}
+
+		public void SelectNextItemSlot()
+		{
+			SelectedItemSlot = (SelectedItemSlot + 1) % InventorySize;
+		}
+
+		public void SelectPreviousItemSlot()
+		{
+			SelectedItemSlot = (SelectedItemSlot + InventorySize - 1) % InventorySize;
+		}
+
+		private int GetNextFreeItemSlot()
+		{
+			int nextFreeItemSlot = -1;
+
+			for (int i = SelectedItemSlot; i < (SelectedItemSlot + InventorySize); i++)
+			{
+				int itemSlot = i % InventorySize;
+
+				if (Items[itemSlot] == null)
+				{
+					nextFreeItemSlot = itemSlot;
+					break;
+				}
+			}
+
+			return nextFreeItemSlot;
 		}
 	}
 }
